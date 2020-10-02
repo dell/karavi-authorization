c0afe83e67c96d9aff649354e71551bf3d6bda24
Ian Bibby <ian.bibby@emc.com>
2020-10-01 22:31:24 -0700

    Reject expired tokens

--- cmd/storage-gatekeeper/main.go
+++ cmd/storage-gatekeeper/main.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -15,6 +16,9 @@ import (
 	"powerflex-reverse-proxy/internal/decision"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 func init() {
@@ -88,6 +92,14 @@ func main() {
 		n, err := strconv.ParseInt(capReq.VolumeSizeInKb, 0, 64)
 		if err != nil {
 			writeError(w, "failed to parse capacity", http.StatusBadRequest)
+			return
+		}
+
+		if true {
+			// Reset the original request
+			r.Body.Close()
+			r.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+			proxy.ServeHTTP(w, r)
 			return
 		}
 
@@ -173,10 +185,51 @@ func main() {
 			}()
 		}
 	})
+
+	//mux.HandleFunc("/api/login", func(w http.ResponseWriter, r *http.Request) {
+	//	log.Println("Proxy clients should not be logging in")
+	//})
 	mux.Handle("/", proxy)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %v", r.RemoteAddr, r.Method, r.URL.Path)
+		authz := r.Header.Get("Authorization")
+		log.Println("Authorization", authz)
+		parts := strings.Split(authz, " ")
+		if len(parts) != 2 {
+			log.Println("invalid authz header")
+			errorResponse(w, http.StatusBadRequest)
+			return
+		}
+		scheme, token := parts[0], parts[1]
+
+		switch scheme {
+		case "Bearer":
+			jwtToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+				return []byte("secret"), nil
+			})
+			if err != nil {
+				log.Printf("parsing token: %+v", err)
+				errorResponse(w, http.StatusUnauthorized)
+				return
+			}
+
+			if claims, ok := jwtToken.Claims.(jwt.StandardClaims); ok && jwtToken.Valid {
+				log.Printf("time.Now() == %v, ExpiresAt == %v", time.Now().Unix(), claims.ExpiresAt)
+				if time.Now().After(time.Unix(claims.ExpiresAt, 0)) {
+					log.Println("Expired token")
+					errorResponse(w, http.StatusUnauthorized)
+					return
+				}
+			}
+
+			setBasicAuth(r)
+		case "Basic":
+			fallthrough
+		default:
+			// nothing to do
+		}
+
 		mux.ServeHTTP(w, r)
 	})
 	//log.Fatal(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil))
@@ -201,4 +254,50 @@ func (w *statusWriter) Write(b []byte) (int, error) {
 	n, err := w.ResponseWriter.Write(b)
 	w.length += n
 	return n, err
+}
+
+func setBasicAuth(req *http.Request) {
+	log.Println("Setting basic auth")
+	authReq, err := http.NewRequest(http.MethodGet, "https://10.247.78.66/api/login", nil)
+	if err != nil {
+		panic(err)
+	}
+	authReq.SetBasicAuth("admin", "Password123")
+	resp, err := http.DefaultClient.Do(authReq)
+	if err != nil {
+		panic(err)
+	}
+	if resp.StatusCode > 299 {
+		panic(err)
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	b = bytes.Trim(b, "\"")
+
+	req.SetBasicAuth("", string(b))
+}
+
+func errorResponse(w io.Writer, httpCode int) {
+	resp := struct {
+		Message        string `json:"message"`
+		HttpStatusCode int    `json:"httpStatusCode"`
+		ErrorCode      int    `json:"errorCode"`
+	}{
+		Message:        http.StatusText(httpCode),
+		HttpStatusCode: httpCode,
+		ErrorCode:      0,
+	}
+	err := json.NewEncoder(w).Encode(&resp)
+	if err != nil {
+		switch t := w.(type) {
+		case http.ResponseWriter:
+			http.Error(t, "encoding failure", http.StatusInternalServerError)
+		default:
+			log.Printf("encoding failed: %+v", err)
+		}
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module powerflex-reverse-proxy
 
 go 1.15
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
