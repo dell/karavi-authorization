9ed87543773cc9480afd6cd270b94ef73e93affa
Ian Bibby <470816+ianbibby@users.noreply.github.com>
2021-02-18 22:21:43 -0800

    Implements the tenant service

--- internal/tenantsvc/service.go
+++ internal/tenantsvc/service.go
@@ -6,11 +6,20 @@ import (
 	"fmt"
 	"karavi-authorization/pb"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/go-redis/redis"
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	ErrTenantAlreadyExists = errors.New("tenant already exists")
+	ErrTenantNotFound      = errors.New("tenant not found")
+	ErrNilTenant           = status.Error(codes.InvalidArgument, "nil tenant")
 )
 
 type TenantService struct {
@@ -52,16 +61,15 @@ func NewTenantService(opts ...Option) *TenantService {
 }
 
 func (t *TenantService) CreateTenant(ctx context.Context, req *pb.CreateTenantRequest) (*pb.Tenant, error) {
-	t.log.Printf("creating tenant: %+v", req)
-
 	key := fmt.Sprintf("tenant:%s", req.Tenant.Name)
+
 	b, err := t.rdb.HSet(key, "created_at", time.Now().Unix()).Result()
 	if err != nil {
 		return nil, err
 	}
 
 	if !b {
-		return nil, errors.New("tenant not created")
+		return nil, ErrTenantAlreadyExists
 	}
 
 	return &pb.Tenant{
@@ -85,16 +93,65 @@ func (t *TenantService) GetTenant(ctx context.Context, req *pb.GetTenantRequest)
 }
 
 func (t *TenantService) UpdateTenant(ctx context.Context, req *pb.UpdateTenantRequest) (*pb.Tenant, error) {
-	t.log.Printf("updating tenant: %+v", req)
+	if req.Tenant == nil {
+		return nil, ErrNilTenant
+	}
+
+	exists, err := t.rdb.Exists(tenantKey(req.Tenant.Name)).Result()
+	if err != nil {
+		return nil, err
+	}
+	if exists == 0 {
+		return nil, ErrTenantNotFound
+	}
+
+	// TODO(ian): Update tenant attributes here.
 	return &pb.Tenant{}, nil
 }
 
 func (t *TenantService) DeleteTenant(ctx context.Context, req *pb.DeleteTenantRequest) (*empty.Empty, error) {
-	t.log.Printf("deleting tenant: %+v", req)
-	return &empty.Empty{}, nil
+	var emp empty.Empty
+	n, err := t.rdb.Del(tenantKey(req.Name)).Result()
+	if err != nil {
+		return &emp, err
+	}
+	if n == 0 {
+		return nil, ErrTenantNotFound
+	}
+
+	return &emp, nil
 }
 
 func (t *TenantService) ListTenant(ctx context.Context, req *pb.ListTenantRequest) (*pb.ListTenantResponse, error) {
-	t.log.Printf("listing tenants: %+v", req)
-	return &pb.ListTenantResponse{}, nil
+	var tenants []*pb.Tenant
+
+	var cursor uint64
+	for {
+		keys, nextCursor, err := t.rdb.Scan(cursor, "tenant:*", 10).Result()
+		if err != nil {
+			return nil, err
+		}
+		for _, v := range keys {
+			split := strings.Split(v, ":")
+			if len(split) != 2 {
+				continue
+			}
+			tenants = append(tenants, &pb.Tenant{
+				Name: split[1],
+			})
+		}
+
+		cursor = nextCursor
+		if cursor == 0 {
+			break
+		}
+	}
+
+	return &pb.ListTenantResponse{
+		Tenants: tenants,
+	}, nil
+}
+
+func tenantKey(name string) string {
+	return fmt.Sprintf("tenant:%s", name)
 }
--- internal/tenantsvc/service_test.go
+++ internal/tenantsvc/service_test.go
@@ -2,6 +2,8 @@ package tenantsvc_test
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"karavi-authorization/internal/tenantsvc"
 	"karavi-authorization/pb"
 	"log"
@@ -11,37 +13,70 @@ import (
 	"github.com/orlangure/gnomock"
 )
 
+type AfterFunc func()
+
 func TestTenantService(t *testing.T) {
 	rdb := createRedisContainer(t)
 	sut := tenantsvc.NewTenantService(tenantsvc.WithRedis(rdb))
 
-	t.Run("CreateTenant", testCreateTenant(sut))
-	t.Run("GetTenant", testGetTenant(sut))
+	afterFn := func() {
+		if _, err := rdb.FlushDB().Result(); err != nil {
+			t.Fatalf("error flushing db: %+v", err)
+		}
+	}
+
+	t.Run("CreateTenant", testCreateTenant(sut, afterFn))
+	t.Run("GetTenant", testGetTenant(sut, afterFn))
+	t.Run("UpdateTenant", testUpdateTenant(sut, afterFn))
+	t.Run("DeleteTenant", testDeleteTenant(sut, afterFn))
+	t.Run("ListTenant", testListTenant(sut, afterFn))
 }
 
-func testCreateTenant(sut *tenantsvc.TenantService) func(*testing.T) {
+func testCreateTenant(sut *tenantsvc.TenantService, afterFn AfterFunc) func(*testing.T) {
 	return func(t *testing.T) {
 		t.Run("it creates a tenant entry", func(t *testing.T) {
+			defer afterFn()
+
 			wantName := "Avengers"
 			got, err := sut.CreateTenant(context.Background(), &pb.CreateTenantRequest{
 				Tenant: &pb.Tenant{
 					Name: wantName,
 				},
 			})
-			if err != nil {
-				t.Fatal(err)
-			}
+			checkError(t, err)
 
 			if got.Name != wantName {
 				t.Errorf("CreateTenant: got name = %q, want %q", got.Name, wantName)
 			}
 		})
+		t.Run("it errors on a duplicate tenant", func(t *testing.T) {
+			first, err := sut.CreateTenant(context.Background(), &pb.CreateTenantRequest{
+				Tenant: &pb.Tenant{
+					Name: "duplicateme",
+				},
+			})
+			checkError(t, err)
+
+			gotTenant, gotErr := sut.CreateTenant(context.Background(), &pb.CreateTenantRequest{
+				Tenant: first,
+			})
+
+			wantErr := tenantsvc.ErrTenantAlreadyExists
+			if gotErr != tenantsvc.ErrTenantAlreadyExists {
+				t.Errorf("CreateTenant: got err = %v, want %v", gotErr, wantErr)
+			}
+			if gotTenant != nil {
+				t.Error("CreateTenant: expected returned tenant to be nil")
+			}
+		})
 	}
 }
 
-func testGetTenant(sut *tenantsvc.TenantService) func(*testing.T) {
+func testGetTenant(sut *tenantsvc.TenantService, afterFn AfterFunc) func(*testing.T) {
 	return func(t *testing.T) {
-		t.Run("created tenant can be gotten", func(t *testing.T) {
+		t.Run("it gets a created tenant", func(t *testing.T) {
+			defer afterFn()
+
 			wantName := "Avengers2"
 			_, err := sut.CreateTenant(context.Background(), &pb.CreateTenantRequest{
 				Tenant: &pb.Tenant{
@@ -60,6 +95,132 @@ func testGetTenant(sut *tenantsvc.TenantService) func(*testing.T) {
 				t.Errorf("GetTenant: got name = %q, want %q", got.Name, wantName)
 			}
 		})
+	}
+}
+
+func testUpdateTenant(sut *tenantsvc.TenantService, afterFn AfterFunc) func(*testing.T) {
+	return func(t *testing.T) {
+		t.Run("it updates an existing tenant", func(t *testing.T) {
+			defer afterFn()
+			tenantName := "testname"
+			_, err := sut.CreateTenant(context.Background(), &pb.CreateTenantRequest{
+				Tenant: &pb.Tenant{
+					Name: tenantName,
+				},
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			got, err := sut.UpdateTenant(context.Background(), &pb.UpdateTenantRequest{
+				Tenant: &pb.Tenant{
+					Name: tenantName,
+				},
+			})
+			checkError(t, err)
+
+			if got == nil {
+				t.Error("UpdateTenant: expected a non-nil tenant value")
+			}
+
+			// TODO(ian): Need to come back to this test once there's more meat on the tenant
+			// struct to update things with.
+		})
+		t.Run("it errors when updating a missing tenant", func(t *testing.T) {
+			defer afterFn()
+
+			gotTenant, gotErr := sut.UpdateTenant(context.Background(), &pb.UpdateTenantRequest{
+				Tenant: &pb.Tenant{
+					Name: "missingtenant",
+				},
+			})
+
+			wantErr := tenantsvc.ErrTenantNotFound
+			if gotErr == nil {
+				t.Errorf("UpdateTenant: got err = %v, want %v", gotErr, wantErr)
+			}
+			if gotTenant != nil {
+				t.Error("UpdateTenant: expected a nil tenant value")
+			}
+		})
+		t.Run("it handles a nil tenant", func(t *testing.T) {
+			defer afterFn()
+
+			_, gotErr := sut.UpdateTenant(context.Background(), &pb.UpdateTenantRequest{})
+
+			wantErr := tenantsvc.ErrNilTenant
+			if !errors.Is(gotErr, wantErr) {
+				t.Errorf("UpdateTenant: got err = %v, want %v", gotErr, wantErr)
+			}
+		})
+	}
+}
+
+func testDeleteTenant(sut *tenantsvc.TenantService, afterFn AfterFunc) func(*testing.T) {
+	return func(t *testing.T) {
+		t.Run("it deletes an existing tenant", func(t *testing.T) {
+			defer afterFn()
+			tenantName := "testname"
+			_, err := sut.CreateTenant(context.Background(), &pb.CreateTenantRequest{
+				Tenant: &pb.Tenant{
+					Name: tenantName,
+				},
+			})
+			checkError(t, err)
+
+			_, err = sut.DeleteTenant(context.Background(), &pb.DeleteTenantRequest{
+				Name: tenantName,
+			})
+			checkError(t, err)
+			_, gotErr := sut.GetTenant(context.Background(), &pb.GetTenantRequest{
+				Name: tenantName,
+			})
+
+			if gotErr != nil {
+				t.Error("DeleteTenant: expected non-nil error")
+			}
+		})
+		t.Run("it errors on a non-existent tenant", func(t *testing.T) {
+			defer afterFn()
+			_, gotErr := sut.DeleteTenant(context.Background(), &pb.DeleteTenantRequest{
+				Name: "doesnotexist",
+			})
+
+			wantErr := tenantsvc.ErrTenantNotFound
+			if gotErr != wantErr {
+				t.Errorf("DeleteTenant: got err %v, want %v", gotErr, wantErr)
+			}
+		})
+	}
+}
+
+func testListTenant(sut *tenantsvc.TenantService, afterFn AfterFunc) func(*testing.T) {
+	return func(t *testing.T) {
+		t.Run("it lists existing tenants", func(t *testing.T) {
+			defer afterFn()
+			for i := 0; i < 5; i++ {
+				_, err := sut.CreateTenant(context.Background(), &pb.CreateTenantRequest{
+					Tenant: &pb.Tenant{
+						Name: fmt.Sprintf("tenant-%d", i),
+					},
+				})
+				checkError(t, err)
+			}
+
+			res, err := sut.ListTenant(context.Background(), &pb.ListTenantRequest{})
+			checkError(t, err)
+
+			wantLen := 5
+			if gotLen := len(res.Tenants); gotLen != wantLen {
+				t.Errorf("got len = %d, want %d", gotLen, wantLen)
+			}
+		})
+	}
+}
+
+func checkError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
