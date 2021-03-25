073b9d81bd1d3271bc9f4fcd98f3e74ddf91af6e
Aaron Tye <tyeaaron@gmail.com>
2021-03-25 18:16:31 -0400

    Add loop to wait for karavi namespace (#64)
    * add loop to wait for karavi namespace
    
    * use K3S variable

--- policies/policy-install.sh
+++ policies/policy-install.sh
@@ -2,8 +2,22 @@
 set -x
 [ $(id -u) -eq 0 ] || exec sudo $0 $@
 
-cd "$(dirname "$0")"
 K3S=/usr/local/bin/k3s
+
+counter=1
+while [[ $($K3S kubectl get namespaces | grep karavi | wc -l) -ne 1 ]]
+do 
+    if [[ "$counter" -eq 30 ]]
+    then
+        echo "karavi namespace not available in k3s"
+        exit 1
+    fi
+    ((counter++))
+    echo "Waiting for karavi namespace in k3s to be available..."
+    sleep 1
+done
+
+cd "$(dirname "$0")"
 $K3S kubectl create configmap common -n karavi --from-file=./common.rego --save-config
 $K3S kubectl create configmap volumes-create -n karavi --from-file=./volumes_create.rego --save-config
 $K3S kubectl create configmap volumes-delete -n karavi --from-file=./volumes_delete.rego --save-config
