60e9440db853e3f0a94e277afe28396d673db1ca
Michael Edegware <michael_edegware@dell.com>
2021-04-29 16:09:44 -0400

    sanitizing and adding injecting functionality

--- cmd/karavictl/cmd/inject_test.go
+++ cmd/karavictl/cmd/inject_test.go
@@ -23,91 +23,31 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-func TestListChange(t *testing.T) {
+func TestListChangePowerFlex(t *testing.T) {
 	// This file was generated using the following command:
 	// kubectl get secrets,deployments,daemonsets -n vxflexos -o yaml
-	b, err := ioutil.ReadFile("./testdata/kubectl_get_all_in_ns_without_karavi.yaml")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var existing corev1.List
-	err = yaml.Unmarshal(b, &existing)
-	if err != nil {
-		t.Fatal(err)
-	}
+	path := "./testdata/kubectl_get_all_in_vxflexos.yaml"
+	listChange(t, path, "vxflexos-config", 5)
+}
 
-	sut := NewListChange(&existing)
-
-	t.Run("injects the proxy pieces", func(t *testing.T) {
-		got, err := injectUsingList(b,
-			"http://image-addr",
-			"http://proxy-addr",
-			"./testdata/fake-certificate-file.pem", false)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if got == nil {
-			t.Error("expected non-nil return value, but got nil")
-		}
-	})
-
-	t.Run("build a map of secrets", func(t *testing.T) {
-		got, err := buildMapOfSecretsFromList(sut.Existing)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		wantLen := 9
-		if l := len(got); l != wantLen {
-			t.Errorf("buildMapOfSecretsFromList: got len %d, want %d", l, wantLen)
-		}
-		// The vxflexos-config Secret should exist with a non-nil value.
-		wantKey := "vxflexos-config"
-		if v, ok := got[wantKey]; !ok || v == nil {
-			t.Errorf("buildMapOfSecretsFromList: expected key %q to exist, but got [%v,%v]",
-				wantKey, v, ok)
-		}
-	})
-	t.Run("inject a new secret with localhost endpoints", func(t *testing.T) {
-		sut.injectKaraviSecret()
-		if sut.Err != nil {
-			t.Fatal(sut.Err)
-		}
-
-		// Extract only the secrets from this list.
-		secrets, err := buildMapOfSecretsFromList(sut.Modified)
-		if err != nil {
-			t.Fatal(err)
-		}
-		secret, ok := secrets["karavi-authorization-config"]
-		if !ok {
-			t.Fatal("expected new secret to exist, but it didn't")
-		}
-		secretData, err := getSecretData(secret)
-		if err != nil {
-			t.Fatal(err)
-		}
-		for _, v := range secretData {
-			u, err := url.Parse(v.Endpoint)
-			if err != nil {
-				t.Fatal(err)
-			}
-			want := "localhost"
-			if got := u.Hostname(); got != want {
-				t.Errorf("got %q, want %q", got, want)
-			}
-		}
-		// The new secret should be called karavi-auth-config
-		// It should replace endpoint values with localhost
-		// Each localhost should have a unique port number
-		// The original secret should be left intact.
-	})
+func TestListChangePowerMax(t *testing.T) {
+	// TODO
+	// This file was generated using the following command:
+	// kubectl get secrets,deployments,daemonsets -n powermax -o yaml
+	path := "./testdata/kubectl_get_all_in_powermax.yaml"
+	listChange(t, path, "powermax-creds", 4)
 }
 
 func TestListChangeObservability(t *testing.T) {
 	// This file was generated using the following command:
-	// kubectl get secrets,deployments,daemonsets -n vxflexos -o yaml
-	b, err := ioutil.ReadFile("./testdata/kubectl_get_all_in_karavi_observability.yaml")
+	// kubectl get secrets,deployments -n karavi -o yaml
+	path := "./testdata/kubectl_get_all_in_karavi_observability.yaml"
+	// TODO: ask Trevor what this is about
+	listChange(t, path, "vxflexos-config", 11)
+}
+
+func listChange(t *testing.T, path, wantKey string, wantLen int) {
+	b, err := ioutil.ReadFile(path)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -138,18 +78,19 @@ func TestListChangeObservability(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		wantLen := 11
 		if l := len(got); l != wantLen {
 			t.Errorf("buildMapOfSecretsFromList: got len %d, want %d", l, wantLen)
 		}
-		// The vxflexos-config Secret should exist with a non-nil value.
-		wantKey := "vxflexos-config"
+		// The Secret should exist with a non-nil value.
 		if v, ok := got[wantKey]; !ok || v == nil {
 			t.Errorf("buildMapOfSecretsFromList: expected key %q to exist, but got [%v,%v]",
 				wantKey, v, ok)
 		}
 	})
 	t.Run("inject a new secret with localhost endpoints", func(t *testing.T) {
+		sut.InjectResources = &Resources{
+			Secret: wantKey,
+		}
 		sut.injectKaraviSecret()
 		if sut.Err != nil {
 			t.Fatal(sut.Err)
@@ -183,13 +124,4 @@ func TestListChangeObservability(t *testing.T) {
 		// Each localhost should have a unique port number
 		// The original secret should be left intact.
 	})
-}
-
-func toSecret(t *testing.T, b []byte) *corev1.Secret {
-	var s corev1.Secret
-	err := yaml.Unmarshal(b, &s)
-	if err != nil {
-		t.Fatal(err)
-	}
-	return &s
 }
