cbeceab29027f2867dfd095fd06550913df42b86
atye <tyeaaron@gmail.com>
2022-05-09 14:20:20 -0400

    remove print

--- internal/role-service/service.go
+++ internal/role-service/service.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"karavi-authorization/internal/role-service/roles"
 	"karavi-authorization/pb"
-	"log"
 	"strings"
 
 	"github.com/sirupsen/logrus"
@@ -134,7 +133,6 @@ func (s *Service) Delete(ctx context.Context, req *pb.RoleDeleteRequest) (*pb.Ro
 
 	matched := make(map[roles.Instance]struct{})
 	existingRoles.Select(func(e roles.Instance) {
-		log.Println(e.RoleKey.String())
 		if strings.Contains(e.RoleKey.String(), roleInstance.RoleKey.String()) {
 			matched[e] = struct{}{}
 		}
