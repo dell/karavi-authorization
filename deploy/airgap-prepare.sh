df154b1f20a3834efe544047643ae408639e8c24
Shayna Finocchiaro <66699024+shaynafinocchiaro@users.noreply.github.com>
2022-05-11 20:22:44 -0400

    update proxy/tenant service version

--- deploy/airgap-prepare.sh
+++ deploy/airgap-prepare.sh
@@ -55,7 +55,7 @@ for image in $(grep "image: docker.io" deployment.yaml | awk -F' ' '{ print $2 }
   docker pull $image
 done
 # Save all referenced images into a tarball.
-grep "image: docker.io" deployment.yaml | awk -F' ' '{ print $2 }' | xargs docker save -o $CRED_SHIELD_IMAGES_TAR
+grep "image: " deployment.yaml | awk -F' ' '{ print $2 }' | xargs docker save -o $CRED_SHIELD_IMAGES_TAR
 
 #Pull all images required to install cert-manager
 for image in $(grep "image: " ${DIST}/$CERT_MANAGER_MANIFEST | awk -F' ' '{ print $2 }' | xargs echo); do
