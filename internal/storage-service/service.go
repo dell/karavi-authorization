0b3486f2c159b04a77e681058564397f0516bab8
Evgeny Uglov <evgeny.uglov@dell.com>
2022-05-13 16:06:33 -0400

    Fix code check errors

--- internal/storage-service/service.go
+++ internal/storage-service/service.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Copyright © 2022 Dell Inc., or its subsidiaries. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -157,3 +158,159 @@ func CheckForDuplicates(ctx context.Context, existingStorages types.Storage, sys
 
 	return nil
 }
+=======
+// Copyright © 2022 Dell Inc., or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"context"
+	"fmt"
+	"karavi-authorization/internal/types"
+	"karavi-authorization/pb"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Option func(*Service)
+
+func defaultOptions() []Option {
+	return []Option{
+		WithLogger(logrus.NewEntry(logrus.New())),
+	}
+}
+
+// WithLogger provides a logger.
+func WithLogger(log *logrus.Entry) func(*Service) {
+	return func(t *Service) {
+		t.log = log
+	}
+}
+
+// Validator validates a role instance
+type Validator interface {
+	Validate(ctx context.Context, systemID string, systemType string, system types.System) error
+}
+
+// Kube operators on storages in Kubernetes
+type Kube interface {
+	GetConfiguredStorage(ctx context.Context) (types.Storage, error)
+	UpdateStorages(ctx context.Context, storages types.Storage) error
+}
+
+type Service struct {
+	kube      Kube
+	validator Validator
+	log       *logrus.Entry
+	pb.UnimplementedStorageServiceServer
+}
+
+func NewService(kube Kube, validator Validator, opts ...Option) *Service {
+	var s Service
+	for _, opt := range defaultOptions() {
+		opt(&s)
+	}
+	for _, opt := range opts {
+		opt(&s)
+	}
+
+	s.kube = kube
+	s.validator = validator
+	return &s
+}
+
+func (s *Service) Create(ctx context.Context, req *pb.StorageCreateRequest) (*pb.StorageCreateResponse, error) {
+	s.log.WithFields(logrus.Fields{
+		"StorageType": req.StorageType,
+		"Endpoint":    req.Endpoint,
+		"SystemId":    req.SystemId,
+		"Username":    req.UserName,
+		"Password":    req.Password,
+	}).Info("Create storage request")
+
+	// Get the current list of registered storage systems
+	s.log.Debug("Getting existing storages")
+	existingStorages, err := s.kube.GetConfiguredStorage(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if existingStorages == nil {
+		existingStorages = make(map[string]types.SystemType)
+	}
+
+	newSystem := types.System{
+		User:     req.UserName,
+		Password: req.Password,
+		Endpoint: req.Endpoint,
+		Insecure: req.Insecure,
+	}
+
+	// Check that we are not duplicating
+	s.log.Debug("Checking for duplicates")
+	err = CheckForDuplicates(ctx, existingStorages, req.SystemId, req.StorageType)
+	if err != nil {
+		return nil, err
+	}
+
+	// Validating storage
+	s.log.Debug("Validating storage")
+	err = s.validator.Validate(ctx, req.SystemId, req.StorageType, newSystem)
+	if err != nil {
+		return nil, err
+	}
+
+	// Creating new storage and adding it to the list of existing storages
+	s.log.Debug("Creating new storage")
+	systemType := existingStorages[req.StorageType]
+	if systemType == nil {
+		systemType = make(map[string]types.System)
+	}
+	systemType[req.SystemId] = newSystem
+	existingStorages[req.StorageType] = systemType
+	err = s.kube.UpdateStorages(ctx, existingStorages)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.StorageCreateResponse{}, nil
+}
+
+func CheckForDuplicates(ctx context.Context, existingStorages types.Storage, systemID string, storageType string) error {
+
+	// Check that we are not duplicating, no errors, etc.
+	sysIDs := strings.Split(systemID, ",")
+	isDuplicate := func() (string, bool) {
+		storType, ok := existingStorages[storageType]
+		if !ok {
+			existingStorages[storageType] = make(map[string]types.System)
+			return "", false
+		}
+		for _, id := range sysIDs {
+			if _, ok = storType[fmt.Sprint(id)]; ok {
+				return id, true
+			}
+		}
+		return "", false
+	}
+
+	if id, result := isDuplicate(); result {
+		err := fmt.Errorf("error: %s system with ID %s is already registered", storageType, id)
+		return err
+	}
+
+	return nil
+}
+>>>>>>> d02e067 (Fix code check errors)
--- internal/storage-service/service_test.go
+++ internal/storage-service/service_test.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Copyright © 2022 Dell Inc., or its subsidiaries. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -162,3 +163,169 @@ type failValidator struct{}
 func (v failValidator) Validate(ctx context.Context, systemID string, systemType string, system types.System) error {
 	return errors.New("error")
 }
+=======
+// Copyright © 2022 Dell Inc., or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage_test
+
+import (
+	"context"
+	"errors"
+	"karavi-authorization/internal/storage-service"
+	"karavi-authorization/internal/types"
+	"karavi-authorization/pb"
+	"testing"
+)
+
+func TestServiceCreate(t *testing.T) {
+
+	// define test input
+	tests := map[string]func(t *testing.T) (*pb.StorageCreateRequest, storage.Validator, storage.Kube, checkFn){
+		"success": func(t *testing.T) (*pb.StorageCreateRequest, storage.Validator, storage.Kube, checkFn) {
+			r := &pb.StorageCreateRequest{
+				StorageType: "powerflex",
+				Endpoint:    "0.0.0.0:443",
+				SystemId:    "542a2d5f5122210f",
+				UserName:    "test",
+				Password:    "test",
+				Insecure:    true,
+			}
+			return r, successfulValidator{}, successfulKube{}, errIsNil
+		},
+		"fail validation": func(t *testing.T) (*pb.StorageCreateRequest, storage.Validator, storage.Kube, checkFn) {
+			r := &pb.StorageCreateRequest{
+				StorageType: "invalid-storage-type",
+				Endpoint:    "0.0.0.0:443",
+				SystemId:    "542a2d5f5122210f",
+				UserName:    "test",
+				Password:    "test",
+				Insecure:    true,
+			}
+			return r, failValidator{}, successfulKube{}, errIsNotNil
+		},
+		"fail kube and validation": func(t *testing.T) (*pb.StorageCreateRequest, storage.Validator, storage.Kube, checkFn) {
+			r := &pb.StorageCreateRequest{
+				StorageType: "powerflex",
+				Endpoint:    "0.0.0.0:443",
+				SystemId:    "542a2d5f5122210f",
+				UserName:    "test",
+				Password:    "test",
+				Insecure:    true,
+			}
+			return r, failValidator{}, failKube{}, errIsNotNil
+		},
+		"fail kube": func(t *testing.T) (*pb.StorageCreateRequest, storage.Validator, storage.Kube, checkFn) {
+			r := &pb.StorageCreateRequest{
+				StorageType: "powerflex",
+				Endpoint:    "0.0.0.0:443",
+				SystemId:    "542a2d5f5122210f",
+				UserName:    "test",
+				Password:    "test",
+				Insecure:    true,
+			}
+			return r, successfulValidator{}, failKube{}, errIsNotNil
+		},
+	}
+
+	// run the tests
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			req, validator, kube, checkFn := tc(t)
+			svc := storage.NewService(kube, validator)
+			_, err := svc.Create(context.Background(), req)
+			checkFn(t, err)
+		})
+	}
+}
+
+func TestCheckForDuplicates(t *testing.T) {
+
+	tests := map[string]func(t *testing.T) (types.Storage, string, checkFn){
+		"Passed_No_Duplicates": func(t *testing.T) (types.Storage, string, checkFn) {
+			existingStorages := types.Storage{
+				"powerflex": types.SystemType{
+					"542a2d5f5122210f": types.System{},
+				},
+			}
+
+			return existingStorages, "different-system-id", errIsNil
+		},
+		"failed systemID exists": func(t *testing.T) (types.Storage, string, checkFn) {
+			existingStorages := types.Storage{
+				"powerflex": types.SystemType{
+					"542a2d5f5122210f": types.System{},
+				},
+			}
+			return existingStorages, "542a2d5f5122210f", errIsNotNil
+		},
+	}
+
+	// run the tests
+	for name, testcase := range tests {
+		t.Run(name, func(t *testing.T) {
+			existingStorages, systemID, checkFn := testcase(t)
+			err := storage.CheckForDuplicates(context.Background(), existingStorages, systemID, "powerflex")
+			checkFn(t, err)
+		})
+	}
+}
+
+// define check functions to pass or fail tests
+type checkFn func(*testing.T, error)
+
+func errIsNil(t *testing.T, err error) {
+	if err != nil {
+		t.Errorf("expected nil err, got %v", err)
+	}
+}
+
+func errIsNotNil(t *testing.T, err error) {
+	if err == nil {
+		t.Errorf("expected non-nil err")
+	}
+}
+
+type successfulKube struct{}
+
+func (k successfulKube) UpdateStorages(ctx context.Context, storages types.Storage) error {
+	return nil
+}
+
+func (k successfulKube) GetConfiguredStorage(ctx context.Context) (types.Storage, error) {
+	return types.Storage{}, nil
+}
+
+type failKube struct{}
+
+func (k failKube) UpdateStorages(ctx context.Context, storages types.Storage) error {
+	return errors.New("error")
+}
+
+func (k failKube) GetConfiguredStorage(ctx context.Context) (types.Storage, error) {
+	return nil, nil
+}
+
+type successfulValidator struct{}
+
+func (v successfulValidator) Validate(ctx context.Context, systemID string, systemType string, system types.System) error {
+	return nil
+}
+
+type failValidator struct{}
+
+func (v failValidator) Validate(ctx context.Context, systemID string, systemType string, system types.System) error {
+	return errors.New("error")
+}
+>>>>>>> d02e067 (Fix code check errors)
