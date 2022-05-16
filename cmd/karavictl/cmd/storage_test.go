8752929e3e00c5e82a415e0e7aa839fd4b64f5d1
Evgeny Uglov <evgeny.uglov@dell.com>
2022-05-16 16:14:08 -0400

    Minor fixes

--- cmd/karavictl/cmd/storage_test.go
+++ cmd/karavictl/cmd/storage_test.go
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // Copyright © 2022 Dell Inc., or its subsidiaries. All Rights Reserved.
-=======
-// Copyright © 2021 Dell Inc., or its subsidiaries. All Rights Reserved.
->>>>>>> 7c87fab (Updated tests)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
