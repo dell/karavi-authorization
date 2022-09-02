c458988db3c9c6d87ff07d8d286a4a53405ce9c3
bjiang27 <105997542+bjiang27@users.noreply.github.com>
2022-09-02 15:55:52 -0400

    Adding flag to skip selinux download (#187)
    * adding flag to skip selinux download
    
    * Modified flag name

--- deploy/install.go
+++ deploy/install.go
@@ -85,6 +85,7 @@ const (
 	RancherK3sKubeConfigPath  = "/etc/rancher/k3s/k3s.yaml"
 	EnvK3sInstallSkipDownload = "INSTALL_K3S_SKIP_DOWNLOAD=true"
 	EnvK3sForceRestart        = "INSTALL_K3S_FORCE_RESTART=true"
+	EnvK3sSkipSelinuxRpm      = "INSTALL_K3S_SKIP_SELINUX_RPM=true"
 )
 
 const (
@@ -736,7 +737,7 @@ func (dp *DeployProcess) ExecuteK3sInstallScript() {
 	}
 
 	cmd := execCommand(filepath.Join(dp.tmpDir, k3SInstallScript))
-	cmd.Env = append(os.Environ(), EnvK3sInstallSkipDownload, EnvK3sForceRestart)
+	cmd.Env = append(os.Environ(), EnvK3sInstallSkipDownload, EnvK3sForceRestart, EnvK3sSkipSelinuxRpm)
 	cmd.Stdout = logFile
 	cmd.Stderr = logFile
 	err = cmd.Run()
