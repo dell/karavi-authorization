96ce87ea94e62bc1761d9bdc76090aca6e52016e
atye <tyeaaron@gmail.com>
2023-02-09 13:52:01 -0500

    fix linting

--- internal/powerflex/client.go
+++ internal/powerflex/client.go
@@ -10,38 +10,38 @@ import (
 // client is the PowerFlex client required for the token getter and storage pool cache
 // The token getter and storage pool cache use the same goscaelio client in different goroutines
 // so we have to protect the client's api token from data races
-type client struct {
+type Client struct {
 	client *goscaleio.Client
 	mu     sync.Mutex // token lock
 }
 
 // NewClient returns a PowerFlex client to be used concurrently with the token getter and storage cache
-func NewClient(c *goscaleio.Client) *client {
-	return &client{
+func NewClient(c *goscaleio.Client) *Client {
+	return &Client{
 		client: c,
 		mu:     sync.Mutex{},
 	}
 }
 
-func (c *client) Authenticate(configConnect *goscaleio.ConfigConnect) (goscaleio.Cluster, error) {
+func (c *Client) Authenticate(configConnect *goscaleio.ConfigConnect) (goscaleio.Cluster, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	return c.client.Authenticate(configConnect)
 }
 
-func (c *client) GetToken() string {
+func (c *Client) GetToken() string {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	return c.client.GetToken()
 }
 
-func (c *client) SetToken(token string) {
+func (c *Client) SetToken(token string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.client.SetToken(token)
 }
 
-func (c *client) FindStoragePool(id string, name string, href string, protectionDomain string) (*types.StoragePool, error) {
+func (c *Client) FindStoragePool(id string, name string, href string, protectionDomain string) (*types.StoragePool, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	return c.client.FindStoragePool(id, name, href, protectionDomain)
