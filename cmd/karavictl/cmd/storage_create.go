1e1d6b7d81c67e3f936f810d5ab8d36896d41a40
root <Isaias.Martinez@Dell.com>
2023-03-29 09:18:32 -0400

    fixed path on create cmd

--- cmd/karavictl/cmd/storage_create.go
+++ cmd/karavictl/cmd/storage_create.go
@@ -462,7 +462,7 @@ func doStorageCreateRequest(addr string, system input, insecure bool, cmd *cobra
 		Insecure:    system.ArrayInsecure,
 	}
 
-	err = client.Post(context.Background(), "/proxy/tenant/create", nil, nil, &body, nil)
+	err = client.Post(context.Background(), "/proxy/storage/create", nil, nil, &body, nil)
 	if err != nil {
 		reportErrorAndExit(JSONOutput, cmd.ErrOrStderr(), err)
 	}
