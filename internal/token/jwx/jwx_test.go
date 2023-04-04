b4047815d87645cb029651c8b165a92786d83662
Shayna Finocchiaro <66699024+shaynafinocchiaro@users.noreply.github.com>
2023-04-04 16:13:19 -0400

    fix format

--- internal/token/jwx/jwx_test.go
+++ internal/token/jwx/jwx_test.go
@@ -153,7 +153,7 @@ func TestParseWithClaims(t *testing.T) {
 
 func TestGenerateAdminToken(t *testing.T) {
 	got, err := jwx.GenerateAdminToken(context.Background(), &pb.GenerateAdminTokenRequest{
-		AdminName: "admin",
+		AdminName:        "admin",
 		JWTSigningSecret: "secret",
 	})
 	if err != nil {
